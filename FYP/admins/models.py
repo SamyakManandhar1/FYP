e995f280f3bbdfa8d559dd5a7da9710677d2af3c
samyak <samyakmanandhar2000@gmail.com>
2022-04-09 11:11:40 +0545

    updated the  admin dashboard page and employee dashboard

--- FYP/admins/models.py
+++ FYP/admins/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+import random
+from django.urls import reverse
+from django.utils import timezone
+import time
 
-# Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=70, null=False, blank=False)
+    history = models.TextField(max_length=1000,null=True,blank=True, default='No History')
+    
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    LANGUAGE = (('english', 'ENGLISH'),
+                ('nepali', 'NEPALI'), ('hindi', 'HINDI'))
+    GENDER = (('male', 'MALE'), ('female', 'FEMALE'), ('other', 'OTHER'))
+    emp_id = models.CharField(
+        max_length=70, default='emp'+str(random.randrange(100, 999, 1)))
+    emp_image = models.ImageField(blank=True, null=True)
+    first_name = models.CharField(max_length=50, null=False)
+    last_name = models.CharField(max_length=50, null=False)
+    mobile = models.CharField(max_length=15)
+    email = models.EmailField(max_length=125, null=False)
+    address = models.TextField(max_length=100, default='')
+    gender = models.CharField(choices=GENDER, max_length=10)
+    department = models.ForeignKey(
+        Department, on_delete=models.SET_NULL, null=True)
+    joined = models.DateTimeField(default=timezone.now)
+    language = models.CharField(
+        choices=LANGUAGE, max_length=10, default='english')
+    accnum = models.CharField(max_length=10, default='0123456789')
+    bank = models.CharField(max_length=25, default='First Bank Plc')
+    salary = models.CharField(max_length=16, default='00,000.00')
+
+    def __str__(self):
+        return self.first_name
+class Attendance (models.Model):
+    STATUS = (('PRESENT', 'PRESENT'), ('ABSENT', 'ABSENT'),('UNAVAILABLE', 'UNAVAILABLE'))
+    date = models.DateField(auto_now_add=True)
+    first_in = models.TimeField()
+    last_out = models.TimeField(null=True)
+    status = models.CharField(choices=STATUS, max_length=15 )
+    staff = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True)
+
+    def save(self,*args, **kwargs):
+        self.first_in = timezone.localtime()
+        super(Attendance,self).save(*args, **kwargs)
+        
+        
+        
+        
+
+
+        
--- FYP/admins/views.py
+++ FYP/admins/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from authentications.auth import admin_only
+from django.contrib.auth.models import User
+from admins.models import Employee,Department
+
 # Create your views here.
 
 
 @login_required
 @admin_only
 def adminDashboard(request):
-  return render(request, 'admins/adminDashboard.html')
+    users = User.objects.all()
+    employee = Employee.objects.all()
+    department = Department.objects.all()
+    
+    employee_count = employee.count()
+    department_count = department.count()
+    admin_count = users.filter(is_superuser=1).count()
+
+    context = {
+        'totalEmployee': employee_count,
+        'totalAdmin': admin_count,
+        'totalDepartment': department_count,
+        'activate_dashboard': 'active bg-dark'
+
+    }
+
+    return render(request, 'admins/adminDashboard.html', context)
